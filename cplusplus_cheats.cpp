96bfbfafc0455b83fa5954f65e7a4033f93b9a99
colerc2 <colerc2@miamioh.edu>
2014-02-01 15:02:58 -0500

    Update cplusplus_cheats.cpp

--- cplusplus_cheats.cpp
+++ cplusplus_cheats.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-
-int main(){
-
-  return 0;
+//read file and store each line in a vector of strings
+void read_file_to_vector(std::vector<std::string> &lines, std::string filename){
+  std::string item;
+  std::ifstream file(filename.c_str());
+  while(std::getline(file, item, '\n')){
+    lines.push_back(item);
+  }
 }
