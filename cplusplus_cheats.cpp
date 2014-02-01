f38647fac29c81c28e0f5bd600e66a6cfa83a65e
colerc2 <colerc2@miamioh.edu>
2014-02-01 15:04:33 -0500

    Update cplusplus_cheats.cpp

--- cplusplus_cheats.cpp
+++ cplusplus_cheats.cpp
@@ -6,3 +6,12 @@ void read_file_to_vector(std::vector<std::string> &lines, std::string filename){
     lines.push_back(item);
   }
 }
+
+//split a string by a specified delimiter into a vector of doubles
+void split_to_double(const std::string &s, char delim, std::vector<double> &elements){
+  std::stringstream ss(s);
+  std::string item;
+  while(std::getline(ss,item,delim)){
+    elements.push_back(atof(item.c_str()));
+  }
+}
