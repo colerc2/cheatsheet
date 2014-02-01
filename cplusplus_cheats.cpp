644ede2d06c4123748d6214c752145cfd11da82c
colerc2 <colerc2@miamioh.edu>
2014-02-01 15:31:46 -0500

    Update cplusplus_cheats.cpp

--- cplusplus_cheats.cpp
+++ cplusplus_cheats.cpp
@@ -15,3 +15,13 @@ void split_to_double(const std::string &s, char delim, std::vector<double> &elem
     elements.push_back(atof(item.c_str()));
   }
 }
+
+
+//parse a given string by a specified delimiter and store in a string vector
+void split_to_string(const std::string &s, char delim, std::vector<std::string> &elements){
+  std::stringstream ss(s);
+  std::string item;
+  while(std::getline(ss,item,delim)){
+    elements.push_back(item);
+  }
+}
