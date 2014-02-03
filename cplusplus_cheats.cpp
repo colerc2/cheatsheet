a5f3bf99bf4199446346bd6fd4fc2466bd8966aa
colerc2 <colerc2@miamioh.edu>
2014-02-03 18:00:33 -0500

    Update cplusplus_cheats.cpp

--- cplusplus_cheats.cpp
+++ cplusplus_cheats.cpp
@@ -1,9 +1,13 @@
 //read file and store each line in a vector of strings
 void read_file_to_vector(std::vector<std::string> &lines, std::string filename){
   std::string item;
-  std::ifstream file(filename.c_str());
-  while(std::getline(file, item, '\n')){
-    lines.push_back(item);
+  std::fstream file(filename.c_str());
+  while(std::getline(file, item)){
+    //find and erase '\r' char used in windows, most painful bug ever
+    if(std::find(item.begin(), item.end(), '\r') != item.end()){
+      item.erase(std::find(item.begin(), item.end(), '\r'));
+    }
+    lines.push_back(std::string(item));
   }
 }
 
